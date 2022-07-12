3f9318d1b2b8422b84ff3d11acc9eff7c32826ef
riquelme <riquelmebandeira1@outlook.com>
2022-07-12 17:20:34 -0300

    feat: study_schedule fn

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+
+    try:
+        for login, logout in permanence_period:
+            if login <= target_time <= logout:
+                counter += 1
+
+    except TypeError:
+        return None
+
+    return counter
