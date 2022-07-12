7fe142222a65ce8ccb2e5081724d4530926e8431
riquelme <riquelmebandeira1@outlook.com>
2022-07-12 18:45:00 -0300

    feat: is_anagram fn

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    word1 = first_string.lower()
+    word2 = second_string.lower()
+
+    if not word1 or not word2:
+        return False
+
+    for letter in word1:
+        if letter not in word2:
+            return False
+        elif word1.count(letter) != word2.count(letter):
+            return False
+
+    return True
