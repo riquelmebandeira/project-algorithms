9a2f3c4f16781475162b737d8b0c603239d1879d
riquelme <riquelmebandeira1@outlook.com>
2022-07-12 18:14:37 -0300

    feat: is_palindrome_recursive fn

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    elif high_index == 0:
+        return True
+    elif low_index == high_index:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
